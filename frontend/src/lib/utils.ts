442be12d3ec3775e2def1c711ecd62f6fdbd1ae4
Solomon Nwante <solomonchuks202@gmail.com>
2025-11-01 17:28:55 +0100

    feat: add yup login schema

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -5,3 +5,12 @@ import * as Yup from 'yup';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const loginSchema = Yup.object({
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('Please enter a valid email address, eg: you@example.com'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters long')
+    .required('Please enter your password'),
+});
