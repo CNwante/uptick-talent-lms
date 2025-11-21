97c68584351b5f525153eef78d8fe7058d4457b4
Oluwanifemi <latunde.nifemi2016@gmail.com>
2025-11-21 11:04:53 +0100

    feat: implement Attendance component and AttendancePage for tracking class participation and history

---
+++ frontend/src/app/student/attendance/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import { Attendance } from '@/features/dashboard/components/Attendance';
+
+export default function AttendancePage() {
+  return <Attendance />;
+}
